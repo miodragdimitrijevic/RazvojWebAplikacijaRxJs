eee5d3cdf44dc754d1e9c5b6bb88dd5c5aa4a392
miodrag_dimitrijevic <miskoodm@elfak.rs>
2019-04-18 04:07:36 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -6,27 +6,3 @@ function prikazi()
     lozinka=lozinka.value ;
     console.log(lozinka);
 }
-const axios = require('axios');
-
-axios.get('http://localhost:3000/korisnik')
-    .then(resp => {
-        data = resp.data;
-        data.forEach(e => {
-            console.log(`${e.id}, ${e.ime}, ${e.lozinka}`);
-        });
-    })
-    .catch(error => {
-        console.log(error);
-    });
-   
-
-axios.post('http://localhost:3000/korisnik', {
-    id: 6,
-    ime: 'Fred',
-    lozinka: 'Blair',
-    poeni: 3
-}).then(resp => {
-    console.log(resp.data);
-}).catch(error => {
-    console.log(error);
-});  
