ec37a3fd0cda74a83b70be771cb7ce25b50a7fb1
miodrag_dimitrijevic <miskoodm@elfak.rs>
2019-04-24 03:05:53 +0200

    pokusaj

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import { async } from "rxjs/internal/scheduler/async";
 
 
 const rngListaBtn=document.getElementById("btnrnglista");
-let flagDaLipostoji=0;
+
+
+
         
 const pomocnatabela=document.getElementById("tabela");
 const btnPocni=document.getElementById("Prijava");
@@ -18,6 +20,9 @@ const buttonHard=document.createElement("button");
 const buttonEasy=document.createElement("button");
 buttonHard.innerHTML="Hard";
 buttonEasy.innerHTML="Easy";
+const nizIdentifikatoraPitanja=[];
+
+const flagObustavljanjaKviza=0;
 
 
 let trenutnikorisnik=null;
@@ -196,6 +201,97 @@ async function crtajTezinuNepostojeci()
     const modalNaslov=document.getElementById("naslovKviz");
     modalNaslov.innerHTML="Izaberite tezinu testa";
 }
+fromEvent(buttonHard,"click").pipe(
+    map(x=>x.target.value)
+).subscribe(tezina=>{pribaviPitanja();});
+
+function randomZaTesko()
+{
+    return Observable.create((randomBr)=>{
+        
+            randomBr.next(parseInt(15*Math.random()+15));
+            
+        
+        
+    }).pipe(filter(x=>x!=14),take(5),debounceTime(50))
+        
+}
+async function pribaviPitanja()
+{
+
+    const bodiKviz=document.getElementById("bodiKviz");
+    const dugmad=document.querySelector(".dugmad");
+    dugmad.hidden=true;
+    const labela=document.createElement("label");
+    labela.innerHTML="Izabrali ste tezak mod kviza..";
+    bodiKviz.appendChild(labela);
+    const modalNaslov=document.getElementById("naslovKviz");
+    modalNaslov.innerHTML="Sacekajte..";
+
+    for(let i=0;i<5;i++)
+    {
+    const sub=randomZaTesko().subscribe(x=> {nizIdentifikatoraPitanja.push(x)});
+    if(i==0)
+    {
+        const pPitanje=await prvoPitanje();
+        axios.get(`http://localhost:3000/pitanja/${nizIdentifikatoraPitanja[i]}`)
+        .then(odg=>
+            {
+                const data=odg.data;
+                modalNaslov.innerHTML=data.ime;
+                labela.hidden=true;
+                axios.get('http://localhost:3000/odgovori')
+                .then(rsp=>
+                {
+                    const pod=rsp.data;
+                    pod.forEach((el)=>{
+                        if(el.id==data.id)
+                        {
+                            if(el.tacnost==1)
+                            {
+                                const tacnoDugme=document.createElement("button");
+                                tacnoDugme.setAttribute("id","1");
+                                tacnoDugme.innerHTML=el.ime;
+                                bodiKviz.appendChild(tacnoDugme);
+                            }
+                        }
+                    })
+
+                })
+                
+               
+            })
+            
+
+    }
+    else{
+    const cekajPromis=await vratiGlupiPromis();
+    console.log(cekajPromis);
+    
+    }
+    }
+}
+
+function vratiGlupiPromis()
+{
+    return new Promise((resolve)=>
+    {
+        setTimeout(()=> {resolve("ovde crtam svako drugo pitanje")},15000);
+    })
+}
+function prvoPitanje()
+{
+    return new Promise((resolve)=>
+    {
+        setTimeout(()=> {resolve("ovde crtam prvo pitanje")},3000);
+    })
+}
+
+
+
+
+
+
 
 
 
